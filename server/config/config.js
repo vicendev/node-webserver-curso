704f08c9f15a95fa7ad2850408794083acaa2e4a
vicendev <vicenmoyano.upla@gmail.com>
2020-05-13 23:48:35 -0400

    Variable entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://admin:rgjWHIvxkRR3TzYx@cluster0-sujbd.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
